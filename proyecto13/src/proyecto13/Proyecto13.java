f48172837fbc1abfa52b5234c1a0cd05b997a89f
edu1542 <31898979+edu1542@users.noreply.github.com>
2017-09-20 17:55:19 -0300

    Add files via upload

--- proyecto13/src/proyecto13/Proyecto13.java
+++ proyecto13/src/proyecto13/Proyecto13.java
@@ -15,23 +15,108 @@ public class Proyecto13 {
 
     /**
      * @param args the command line arguments
-     * se ingresa una edad y se informa si es mayor de edad
+     * 
+     * 
+     * scanner misc = new scanner(system.in);
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int edad;
+        Scanner misc = new Scanner(System.in);
         
-        Scanner miescanner = new Scanner(System.in);
+        String nombre;
+        String clave;
+        String sexo;
+        String localidad;
+        String color;
+
+
+
+        System.out.println("ingrese nombre: ");
+
+
+
+
+
+
+
+        nombre = misc.next();
+
+        System.out.println("ingrese clave: ");
+        clave = misc.next();
         
-        System.out.println("ingrese edad");
-        edad = miescanner.nextInt();
+        System.out.println("ingrese sexo: ");
+        sexo = misc.next();
+        //ingrese m para masculino , f para femenimo
+        System.out.println("ingrese localidad: ");
+        localidad = misc.next();
         
-        if(edad > 17){
-            System.out.println("es mayor");
-            
+        
+        System.out.println("ingrese color: ");
+        color = misc.next();
+        /**
+         * 1si es masculino q viva en la lanus en avellaneda y q su color 
+         * favorito sea rojo mostrar el msj sos de independiente
+         * 2si el nombre es andrea ,la localidad es moron , el color favorito es 
+         * verde y azul , msj feliz primavera
+         */
+          
+         if(sexo.equals("m")&&color.equals("rojo")){
+         
+             if(localidad.equals("lanus")||localidad.equals("avellaneda")){
+             
+                 System.out.println("sos de independiente");
+             }
+         }
+             
+          if(nombre.equals("andrea")&&localidad.equals("moron")){
+         
+             if(color.equals("verde")||color.equals("azul")){
+             
+                 System.out.println("feliz primavera");
+             }
+         }
+         
+        if(color.equals("blanco") || color.equals("celeste"))
+        {
+            System.out.println("sos un patriota");
         }
-      
+        
+        if(sexo.equals("f")&&localidad.equals("lanus"))
+            {
+                System.out.println("bienvenida chica de lanus");
+            }
+        
+        if(sexo.equals("f"))
+        {
+            System.out.println("feliz dia de la mujer");
+            
+//            if(localidad.equals("lanus")){
+//                
+//                System.out.println("bienvenida chica de lanus");
+//                
+//            }
+        }    
+    
+        
+        if(nombre.equals("admin"))
+        {
+            System.out.println("bienvenido administrador.");    
+        } 
+        else
+        {
+            System.out.println("bienvenido "+ nombre);
+            
+        
+        }
+        
+    
+    
+    }
+        
+        
+        
+        
         
     }
     
-}
+
